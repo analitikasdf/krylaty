51cc323550c4a19c680dc78caf84ed426516965c
Dmitrii Barsukov <analitik.asdf@gmail.com>
2022-02-08 11:46:59 +0300

    базовая структура страниц и шаблон

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h2>Шапка</h2>
+		<AppHeader />
+		<h2>подвал</h2>
+	</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ pages/home.vue
@@ -0,0 +1,9 @@
+<template>
+	<AppHeader />
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
